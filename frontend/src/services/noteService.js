8286ed20a5970bd965fd52dd704c5518a1844b93
66160116 <66160116@go.buu.ac.th>
2026-03-22 14:27:26 +0000

    Edit noteService.js

--- frontend/src/services/noteService.js
+++ frontend/src/services/noteService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.VUE_APP_API_URL || "http://10.5.3.213:31476/api/notes";
+const API_URL = process.env.VUE_APP_API_URL || "http://10.5.3.213:30000/api/notes";
 
 export default {
   serverDown() {
